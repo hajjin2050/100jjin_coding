d13c696be07f816f5b38a1eb54577f71cef5dbde
moonsh2050 <76682924+moonsh2050@users.noreply.github.com>
2021-10-19 18:23:43 +0900

    Create while_10951번_A+B-4.py

---
+++ while_10951번_A+B-4.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
+        
+  #테스트 케이스 개수가 정해지지 않았기때문에 Try, except 사용!
+  #나머지는 10952번과 
